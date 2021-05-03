2794c57d918f4198d7dd9dfcfe36da8ba2a3c4d3
mohdarshad1 <mdarshad181@gmail.com>
2021-05-03 17:05:45 +0530

    [Mohd Arshad] : Add Number Play File

--- src/com/NumberPlay/NumberPlayList.java
+++ src/com/NumberPlay/NumberPlayList.java
@@ -14,10 +14,12 @@ public class NumberPlayList {
 		List<Integer> list=new ArrayList<>();
 		for(int i=0;i<5;i++)
 			list.add(i);
+		
 		//Method 1 traversing using iterator
 		Iterator<Integer> itr=list.iterator();
 		while(itr.hasNext())
 			System.out.println("Iterator value "+itr.next());
+		
 		//Method 2 Traversing with consumer interface implementation
 		class MyConsumer implements Consumer<Integer>{
 			public void accept(Integer t) {
@@ -26,24 +28,29 @@ public class NumberPlayList {
 		}
 		MyConsumer action=new MyConsumer();
 		list.forEach(action);
+		
 		//Method 3 Traversing with anonomys consumer interface
 		list.forEach(new Consumer<Integer>() {
 			public void accept(Integer t) {
 				System.out.println("Method3 Consumer anonomys impl value "+t);
 			}
 		});
+		
 		//Method 4 explicit lamba exp
 		Consumer<Integer> listAction=n->{
 			System.out.println("Method4 foreach lambda impl value "+n);
 		};
 		list.forEach(listAction);
-		//Method 5 implicit lmbda exp
+		
+		//Method 5 implicit lambda exp
 		list.forEach(n->{System.out.println("Method5 foreach lambda impl value "+n);});
+		
 		//Method 6 implicit lambda exp double
 		Function<Integer,Double> doubleFun=Integer::doubleValue; //n->n.doubleValue();
 		list.forEach(n->{
 			System.out.println("Method6 foreach lambda impl value "+doubleFun.apply(n));
 			});
+		
 		//Method 7 implicit lambda even check
 		Predicate<Integer> isEven=n->n>0 && n%2==0;
 		list.forEach(n->{
@@ -54,12 +61,14 @@ public class NumberPlayList {
 		list.stream().forEach(n->{
 			System.out.println("Method 8: "+isEven.test(n));
 		});
+		
 		//Method 9 double value stream
 		List<Double> streamList=list.stream()
 				.filter(isEven)
 				.map(doubleFun)
 				.collect(Collectors.toList());
 		System.out.println("Method 9 Even List: "+streamList);
+		
 		//Method 10 first even
 		Integer first=list.stream()
 				.filter(isEven)
@@ -67,28 +76,32 @@ public class NumberPlayList {
 				.findFirst()
 				.orElse(null);
 		System.out.println(" Method 10 First even : "+first);
+		
 		//Method 11 min even
 		Integer minEven=list.stream()
 				.filter(isEven)
 				.min(Comparator.comparing(Integer::intValue))
 				.orElse(null);
 		System.out.println("Method 11 Min even: "+minEven);
+		
 		//Method 12 max even
 		Integer max=list.stream()
 					.filter(isEven)
 					.max(Comparator.comparing(Integer::intValue))
-					.orElse(null);
-					
-				System.out.println("Method 12 Max even: "+max);
+					.orElse(null);		
+		System.out.println("Method 12 Max even: "+max);
+	   
 	   //Method 13 Sum,count and avg
 	   Integer sum=list.stream().reduce(0, Integer::sum);
 	   long count=list.stream().count();
 	   System.out.println("Method 13 sum is: "+sum+" count is: "+count+" Average is: "+sum/count);
+	   
 	   //Method 14 checking all even, single even and none divisible by 6
 	   boolean allEven=list.stream().allMatch(isEven);
 	   boolean oneEven=list.stream().anyMatch(isEven);
 	   boolean noneMulSix=list.stream().noneMatch(i->i>0 && i%6==0);
 	   System.out.println("Method 14 alleven: "+allEven+" one even: "+oneEven+" none divisible by 6: "+noneMulSix);
+	   
 	   //Method 15 sort ascending
 	   List<Integer> sortedLs=list.stream()
 			   .sorted((n1,n2)->n1.compareTo(n2))
